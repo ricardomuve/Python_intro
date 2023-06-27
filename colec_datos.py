275d632cbb6a1e01ce7a24bbfe70ab9b378e5b56
ricardomuve <129324603+ricardomuve@users.noreply.github.com>
2023-06-27 12:52:16 -0600

    Create colec_datos.py

---
+++ colec_datos.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+"""
+## Tipo de colección de datos
+Created on Tue Jun 27 11:05:40 2023
+@author: searchman
+"""
+# Lista heterogénea(distintos tipos de datos)
+lista=["Lunes","Martes","Miércoles","Jueves",
+       "Viernes","Sábado","Domingo", True]
+
+# Imprimir el contenido de la lista 'Lista'
+print(lista)
+
+# Imprimir el contenido de 'Lista[1]=Martes'
+print(lista[1])
+
+# Imprimir el contenido de 'Lista[-1]=True'
+print(lista[-1])    
+
+# Longitud de 'Lista' con método 'len()'
+print(len(lista))
+
+# Lista2
+nombres=["Juan","Héctor",1,2,3,False,7.1]
+print(nombres[-2])
+print(nombres[5])
+print(len(nombres))
+
+# Modificando la lista 'nombres[1]=Héctor'
+nombres[1]=3
+print(nombres)
+
+
+
+
+
+
+
+
+
