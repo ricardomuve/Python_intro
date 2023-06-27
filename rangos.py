add8264ae234711c44ce48e7537e0f3afde7a81f
ricardomuve <129324603+ricardomuve@users.noreply.github.com>
2023-06-27 12:50:55 -0600

    Create rangos.py

---
+++ rangos.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+## Rangos
+Created on Tue Jun 27 12:32:11 2023
+@author: searchman
+"""
+
+## Usando rangos
+lista_nueva=[1,2,3,4,5,6,7]
+
+## Ciclo for usando una lista como iterador rango=6
+for i in range(len(lista_nueva)):
+    print(lista_nueva[i])
+    
+## Ciclo for usando un rango de 0 a 6
+for i in range(0,6):
+    print(lista_nueva[i])
+    
+## Ciclo for usando un rango de 0 a 6, de 2en2
+for i in range(0,6,2):
+    print(lista_nueva[i])
+    
+## Ciclo for usando un rango de 1 a 6
+for i in range(1,7):
+    print(lista_nueva[i])
